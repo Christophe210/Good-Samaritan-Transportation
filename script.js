366632c33f2f884a78541d4a0b7d29a54271a21d
Christophe <128588828+Christophe210@users.noreply.github.com>
2023-10-30 14:59:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menubar = document.querySelector('#menu-bars')
+let mynav = document.querySelector('.navbar');
+
+menubar.onclick = () =>{
+    menubar.classList.toggle('fa-times');
+    mynav.classList.toggle('active');
+}
